42fc4a3734063720557cc8c1dcdcaa7a14aea4e7
Omkar <omkarborse3@gmail.com>
2026-02-12 00:17:56 +0530

    permission for internet

--- triviax/lib/data/services/api_service.dart
+++ triviax/lib/data/services/api_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
+
 import '../models/question_model.dart';
 
 class ApiService {
-  final String _baseUrl = 'https://the-trivia-api.com/v2/questions';
+  final String _baseUrl = 'https://the-trivia-api.com/api/questions';
   final Logger _logger = Logger();
 
   Future<List<Question>> fetchQuestions({
@@ -12,13 +14,17 @@ class ApiService {
     String difficulty = 'easy',
   }) async {
     try {
-      final url = Uri.parse('$_baseUrl?limit=$limit&difficulties=$difficulty');
-      _logger.i('Fetching questions from: $url');
+      final url = Uri.parse('$_baseUrl?limit=$limit&difficulty=$difficulty');
 
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
+        final dynamic decodedData = json.decode(response.body);
+        if (decodedData is! List) {
+          throw Exception('Invalid response format');
+        }
+
+        final List<dynamic> data = decodedData;
         return data.map((json) => Question.fromJson(json)).toList();
       } else {
         _logger.e('Failed to fetch questions: ${response.statusCode}');
